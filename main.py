82868a5447a52e0617425cca481e6b1e4ff718aa
Toni59400 <95470541+Toni59400@users.noreply.github.com>
2022-09-05 15:02:40 +0200

    recup

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import pygame
+import classGame
+
 pygame.init()
 
 # générer la fenêtre de notre jeu
 pygame.display.set_caption("WorkShop2023")
 screen = pygame.display.set_mode((1280, 720))
 jeu = True
-game = GamePingpong(screen, 3)
+game = Game(screen, 3)
 
 while jeu:
     pygame.display.flip()
