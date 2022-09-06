3058d31bd5787329a01eefc9f5c00c64f8871530
Toni59400 <95470541+Toni59400@users.noreply.github.com>
2022-09-06 18:07:29 +0200

    0609 commit toni

--- classGame.py
+++ classGame.py
@@ -9,6 +9,7 @@ class Game:
         """
         screen - pygame.Screen - l'écran sur lequel s'affiche le jeu"""
         self.screen = screen
+        self.n_question = 0
         self.pixel = (self.screen.get_width()/384, self.screen.get_height()/216)
         self.background = pygame.transform.scale(pygame.image.load('assets/background.png'),(384*self.pixel[0],216*self.pixel[1]))
         self.spectateur = Spectateur(self, self.screen, self.pixel)
@@ -19,8 +20,8 @@ class Game:
         #exemple son
         #self.soundEnd = pygame.mixer.Sound("jeuPingPong/assets/sound/applaudissements.mp3")
         #Creationliste des question
-        dict_question = [{"question": "« Les personnes entièrement vaccinées développent le syndrome d’immunodéficience acquise (sida) »", "r1" : "Vrai", "r2" : "Faux", "bonne_reponse" : 2, "explication" : "Cette FakeNews provient du site 'Le Grand Réveil', et elle aurait pour source : 'Les rapports officiels du gouvernement britanique'"},
-                 {"question": "« Le vaccin contre le Covid-19 contiendrait des puces électroniques 5G, pour nous tracer et ficher ! »", "r1" : "Vrai", "r2": "Faux", "bonne_reponse" : 2, "explication" : " Cette FakeNews est relayer sur les reseaux, démentie par le CHU de Toulouse dans un tweet."}
+        dict_question = [{"question": "Les personnes entièrement vaccinées développent le syndrome d’immunodéficience acquise (sida).", "r1" : "Vrai", "r2" : "Faux", "bonne_reponse" : 2, "explication" : "Cette FakeNews provient du site 'Le Grand Réveil', et elle aurait pour source : 'Les rapports officiels du gouvernement britanique'"},
+                 {"question": "Le vaccin contre le Covid-19 contiendrait des puces électroniques 5G, pour nous tracer et ficher !", "r1" : "Vrai", "r2": "Faux", "bonne_reponse" : 2, "explication" : " Cette FakeNews est relayer sur les reseaux, démentie par le CHU de Toulouse dans un tweet."}
                 ]
 
         self.lst_obj_question = []
@@ -39,6 +40,6 @@ class Game:
         self.ecran.update()
         self.spectateur.update()
         self.presentateur.update()
-        self.lst_obj_question[0].update()
+        self.lst_obj_question[1].update()
         
 
--- classQuestion.py
+++ classQuestion.py
@@ -1,10 +1,13 @@
+from tkinter import Label
 import pygame
+import textwrap
 
 class Question():
     '''
     
     '''
     def __init__(self, game, screen, question, reponse, propositions, explication):
+        self.white = (255,255,0)
         self.screen = screen 
         self.question = question #string
         self.reponse = reponse #index dans la liste proposition
@@ -13,9 +16,26 @@ class Question():
         self.game = game
         self.pixel = game.pixel
         self.img = pygame.transform.scale(pygame.image.load('assets/presentateur/bulle.png'),(384*self.pixel[0],216*self.pixel[1]))
+        self.reponse1 = pygame.transform.scale(pygame.image.load('assets/presentateur/carreRep.png'), (96*self.pixel[0], 64*self.pixel[1]))
+        self.reponse2 = pygame.transform.scale(pygame.image.load('assets/presentateur/carreRep.png'), (96*self.pixel[0], 64*self.pixel[1]))
+        self.arial_font = pygame.font.SysFont("arial", round(8*self.pixel[1]))
+        self.rectangle = pygame.draw.rect(self.screen, self.white, pygame.Rect(30*self.pixel[0], 25*self.pixel[1], 250*self.pixel[0], 68*self.pixel[1]), width=0)
 
     def update(self):
         """cette fonction met à jour l'affichage et la position des spectateurs"""
+        self.black = (0,0,0)
+        self.rep1 = self.arial_font.render(self.propositions[0], False, self.black)
+        self.rep2 = self.arial_font.render(self.propositions[1], False, self.black)
         self.screen.blit(self.img, (0,0))
-
-    
+        # prochaine ligne = rectangle avec les reponses affichées dedans 
+        self.screen.blit(self.reponse1, (20*self.pixel[0], 140*self.pixel[1]))
+        self.screen.blit(self.rep1, (25*self.pixel[0], 145*self.pixel[1]))
+        self.screen.blit(self.reponse1, (140*self.pixel[0], 140*self.pixel[1]))
+        self.screen.blit(self.rep2, (145*self.pixel[0], 145*self.pixel[1]))
+        width_max = 64
+        wrapped_lines = textwrap.wrap(self.question, width_max)
+        c=0
+        for i in wrapped_lines : 
+            self.text_question = self.arial_font.render(i, False, self.black)
+            self.screen.blit(self.text_question, [30*self.pixel[0], 25*self.pixel[1]+(10*self.pixel[1])*c])
+            c+=1
