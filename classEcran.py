28ab24e5a69c1e46bb00e440a70527ff63625caf
ThiBalt <thibalt.meurin@laposte.net>
2022-09-07 09:57:42 +0200

    dessins + ajout structure

--- classEcran.py
+++ classEcran.py
@@ -8,12 +8,16 @@ class Ecran:
         self.screen = screen
         self.game = game
         self.pixel = pixel
-        self.imgOrigin = pygame.transform.scale(pygame.image.load('assets/ecran/ecran.png'),(384*self.pixel[0],216*self.pixel[1]))
+        self.imgOrigin = pygame.transform.scale(pygame.image.load('assets/ecran/ecran0.png'),(384*self.pixel[0],216*self.pixel[1]))
         self.img = self.imgOrigin
+        self.imgOriginStructure = pygame.transform.scale(pygame.image.load('assets/ecran/structure.png'),(384*self.pixel[0],216*self.pixel[1]))
+        self.imgStructure = self.imgOrigin
 
     def update(self):
         """cette fonction met à jour l'affichage et la position des spectateurs"""
         self.screen.blit(self.img, (self.pixel[0]*192 - self.img.get_width()/2, self.pixel[1]*108 - self.img.get_height()/2 + 68*self.pixel[1]/100*self.game.zoom))
+        self.screen.blit(self.imgStructure, (self.pixel[0]*192 - self.img.get_width()/2, self.pixel[1]*108 - self.img.get_height()/2 + 68*self.pixel[1]/100*self.game.zoom))
 
     def zoom(self, n):
         self.img = pygame.transform.rotozoom(self.imgOrigin,0,1+(n*(2.5/255)))
+        self.imgStructure = pygame.transform.rotozoom(self.imgOriginStructure,0,1+(n*(2.5/255)))
